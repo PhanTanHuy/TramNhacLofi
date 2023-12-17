a12fe3fd79f1be0f5c0f2b50e46eeeed6ff7bb9f
PhanTanHuy <phantanhuy265@gmail.com>
2023-12-17 23:42:06 +0700

    push

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ function Music(name, artist, imageSrc, mp3Src) {
 
 var playlist = [
     new Music("Id 072019 | 3107 ft 267", "W/n","image/img1.jfif" ,"music/song1.mp3"),
-    new Music("Fire Force - Opening | Inferno", "W/n","image/img2.jfif" ,"music/song2.mp3"),
+    new Music("Fire Force - Opening | Inferno", "Fire Force","image/img8.jfif" ,"music/song2.mp3"),
     new Music("W/n - 3107 3 ft. 267", "W/n","image/img3.jpg" ,"music/song3.mp3"),
-    new Music("WREN EVANS - TỪNG QUEN", "W/n","image/img4.jfif" ,"music/song4.mp3"),
-    new Music("Dragon Ball GT (AMV) - Dan Dan Kokoro Hikareteku", "W/n","image/img5.jpg" ,"music/song5.mp3"),
-    new Music("Tokyo Ghoul - Opening 1 | Unravel", "W/n","image/img7.jfif" ,"music/song6.mp3"),
-    new Music("W/n - 3107 4 x Erik ft Nâu", "W/n","image/img8.jfif" ,"music/song7.mp3"),
-    new Music("Nom - Beneath The Rain", "W/n","image/img9.jpg" ,"music/song8.mp3")
+    new Music("WREN EVANS - TỪNG QUEN", "WREN EVANS","image/img4.jfif" ,"music/song4.mp3"),
+    new Music("Dan Dan Kokoro Hikareteku", "Dragon Ball GT","image/img5.jpg" ,"music/song5.mp3"),
+    new Music("Tokyo Ghoul - Opening 1 | Unravel", "Tokyo Ghoul ","image/img7.jfif" ,"music/song6.mp3"),
+    new Music("W/n - 3107 4 x Erik ft Nâu", "W/n","image/img2.jfif" ,"music/song7.mp3"),
+    new Music("Nom - Beneath The Rain", "Nom","image/img9.jpg" ,"music/song8.mp3")
 ];
   
 var audio = new Audio(); // Đối tượng âm thanh
@@ -30,19 +30,31 @@ function playMusic(button) {
     var songName = button.getAttribute("data-song");
     var playIcon = button.querySelector("i");
     var backgroundCurrentSong = button.parentNode.parentNode;
+    var changerightPlay = document.getElementById('rightPlay');
+     // lấy giá trị giao diện bên phải
     var circle = document.getElementById('animationCircle');
+    var tenbaihat = document.getElementById('tenbaihat');
+    var tacgia = document.getElementById('tacgia');
+    var anhnen = document.getElementById('anhnen');
+    
     if (isPlaying) {
         if (currentSong != songName) {
             audio.pause();
             id2.classList.remove("fa-pause");
             bgrCurSong.classList.remove("currentSong");
             id2.classList.add("fa-play");
+            changerightPlay.classList.add("fa-play");
+            changerightPlay.classList.remove("fa-pause");
             console.log('đang dừng bài'+ currentSong);
             circle.classList.remove('cdCricle');
             console.log('da xoa circle');
             isPlaying = false;
+            // đổi giao diện bên phải
+
         } else {
             audio.pause();
+            changerightPlay.classList.add("fa-play");
+            changerightPlay.classList.remove("fa-pause");
             playIcon.classList.remove("fa-pause");
             playIcon.classList.add("fa-play");
             bgrCurSong.classList.remove("currentSong");
@@ -50,6 +62,7 @@ function playMusic(button) {
             console.log('đang dừng bài'+ currentSong);
             isPlaying = false;
             return;
+             // đổi giao diện bên phải
         }
     }     
     for (var i=0; i<playlist.length; i++) {
@@ -59,23 +72,27 @@ function playMusic(button) {
             if (!isPlaying) {
                 audio.src = playlist[i].mp3Src;
                 audio.play();
-                isPlaying = true;
+                isPlaying = true;     
                 playIcon.classList.remove("fa-play");
                 playIcon.classList.add("fa-pause");
                 circle.style.animationPlayState = 'running';
                 backgroundCurrentSong.classList.add("currentSong");
                 console.log('đang phát bài'+ songName);
                 currentSong = songName;
-                
+                 // đổi giao diện bên phải
+                anhnen.style.backgroundImage = `url("${playlist[i].imageSrc}")`;
+                tenbaihat.innerHTML = playlist[i].name;
+                tacgia.innerHTML = playlist[i].artist;
+                circle.style.backgroundImage = `url("${playlist[i].imageSrc}")`;
             }
             break; 
         }
     }
-    rightCol(audio);
+    rightCol(audio, playIcon);
     id2 = playIcon;
     bgrCurSong = backgroundCurrentSong;
 }
-function rightCol(audio) {
+function rightCol(audio, playIcon) {
     console.log(audio); 
     var rightPlay = document.getElementById('rightPlay');
     var animation = document.getElementById('animationCircle');
@@ -85,6 +102,8 @@ function rightCol(audio) {
         console.log('da click');
         if (!audio.paused) {
             audio.pause();
+            playIcon.classList.add("fa-play");
+            playIcon.classList.remove("fa-pause");
             rightPlay.classList.add("fa-play");
             rightPlay.classList.remove("fa-pause");
             animation.style.animationPlayState = 'paused';
@@ -92,13 +111,23 @@ function rightCol(audio) {
         }
         else {
             audio.play();    
+            playIcon.classList.add("fa-pause");
+            playIcon.classList.remove("fa-play");
             rightPlay.classList.remove("fa-play");
             rightPlay.classList.add("fa-pause");
             console.log('da phat nhac roi');
             animation.style.animationPlayState = 'running';
         }
     }
-
+    // lắng nghe sự kiện nhạc hết
+    audio.addEventListener('ended', function() {
+        // Ngưng quay ảnh
+        animation.style.animationPlayState = 'paused';
+        rightPlay.classList.add("fa-play");
+        rightPlay.classList.remove("fa-pause");
+        playIcon.classList.add("fa-play");
+        playIcon.classList.remove("fa-pause");
+    });
 
     // xử lí thanh tiến trình nhạc 
     var progressBar = document.getElementById('thanh_trangthai');
