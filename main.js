2ab7a06651584c5bf4dfc67377fe5116687eca08
PhanTanHuy <phantanhuy265@gmail.com>
2023-12-18 08:58:51 +0700

    finish

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ var playlist = [
     new Music("Dan Dan Kokoro Hikareteku", "Dragon Ball GT","image/img5.jpg" ,"music/song5.mp3"),
     new Music("Tokyo Ghoul - Opening 1 | Unravel", "Tokyo Ghoul ","image/img7.jfif" ,"music/song6.mp3"),
     new Music("W/n - 3107 4 x Erik ft Nâu", "W/n","image/img2.jfif" ,"music/song7.mp3"),
-    new Music("Nom - Beneath The Rain", "Nom","image/img9.jpg" ,"music/song8.mp3")
+    new Music("Nom - Beneath The Rain", "Nom","image/img9.jpg" ,"music/song8.mp3"),
+    new Music("Chozetsu Dynamic!", "Dragonball Super","image/goku.jfif" ,"music/song9.mp3"),
+    new Music("Rồng thiêng", "Dragonball Kai","image/goku2.png" ,"music/song10.mp3")
 ];
   
 var audio = new Audio(); // Đối tượng âm thanh
@@ -28,7 +30,7 @@ var bgrCurSong = null;
 
 function playMusic(button) {
     var songName = button.getAttribute("data-song");
-    var playIcon = button.querySelector("i");
+    // var playIcon = button.querySelector("i");
     var backgroundCurrentSong = button.parentNode.parentNode;
     var changerightPlay = document.getElementById('rightPlay');
      // lấy giá trị giao diện bên phải
@@ -40,9 +42,9 @@ function playMusic(button) {
     if (isPlaying) {
         if (currentSong != songName) {
             audio.pause();
-            id2.classList.remove("fa-pause");
+            // id2.classList.remove("fa-pause");
             bgrCurSong.classList.remove("currentSong");
-            id2.classList.add("fa-play");
+            // id2.classList.add("fa-play");
             changerightPlay.classList.add("fa-play");
             changerightPlay.classList.remove("fa-pause");
             console.log('đang dừng bài'+ currentSong);
@@ -55,8 +57,8 @@ function playMusic(button) {
             audio.pause();
             changerightPlay.classList.add("fa-play");
             changerightPlay.classList.remove("fa-pause");
-            playIcon.classList.remove("fa-pause");
-            playIcon.classList.add("fa-play");
+            // playIcon.classList.remove("fa-pause");
+            // playIcon.classList.add("fa-play");
             bgrCurSong.classList.remove("currentSong");
             circle.style.animationPlayState = 'paused';
             console.log('đang dừng bài'+ currentSong);
@@ -64,17 +66,17 @@ function playMusic(button) {
             return;
              // đổi giao diện bên phải
         }
-    }     
-    for (var i=0; i<playlist.length; i++) {
-       
-        
+    }
+    var index = null;
+    for (var i=0; i<playlist.length; i++) {     
         if (playlist[i].name === songName) {
+            index = i;
             if (!isPlaying) {
                 audio.src = playlist[i].mp3Src;
                 audio.play();
                 isPlaying = true;     
-                playIcon.classList.remove("fa-play");
-                playIcon.classList.add("fa-pause");
+                // playIcon.classList.remove("fa-play");
+                // playIcon.classList.add("fa-pause");
                 circle.style.animationPlayState = 'running';
                 backgroundCurrentSong.classList.add("currentSong");
                 console.log('đang phát bài'+ songName);
@@ -88,11 +90,53 @@ function playMusic(button) {
             break; 
         }
     }
-    rightCol(audio, playIcon);
-    id2 = playIcon;
+    var next = document.getElementById('next');
+    var back = document.getElementById('back');
+    // xu li khi doi bai hat
+    next.addEventListener('click', function(){
+        if (isPlaying){
+            audio.pause();
+        }
+        if (index === playlist.length-1){
+            index = -1;
+        }
+        // doi giao dien va bai hat
+        audio.src = playlist[index + 1].mp3Src;
+        anhnen.style.backgroundImage = `url("${playlist[index + 1].imageSrc}")`;
+        tenbaihat.innerHTML = playlist[index + 1].name;
+        tacgia.innerHTML = playlist[index + 1].artist;
+        circle.style.backgroundImage = `url("${playlist[index + 1].imageSrc}")`;
+        console.log(audio);
+        audio.play();
+        index++;
+    })
+    back.addEventListener('click', function(){
+        if (isPlaying){
+            audio.pause();
+        }
+        if (index === 0){
+            index = playlist.length;
+        }
+        // doi giao dien va bai hat
+        audio.src = playlist[index - 1].mp3Src;
+        anhnen.style.backgroundImage = `url("${playlist[index - 1].imageSrc}")`;
+        tenbaihat.innerHTML = playlist[index - 1].name;
+        tacgia.innerHTML = playlist[index - 1].artist;
+        circle.style.backgroundImage = `url("${playlist[index - 1].imageSrc}")`;
+        console.log(audio);
+        audio.play();
+        index--;
+
+    })
+
+
+    rightCol(audio);
+    // id2 = playIcon;
     bgrCurSong = backgroundCurrentSong;
 }
-function rightCol(audio, playIcon) {
+function rightCol(audio) {
+    var statusSong = document.getElementById('statusSong');
+    statusSong.style.display = 'block';
     console.log(audio); 
     var rightPlay = document.getElementById('rightPlay');
     var animation = document.getElementById('animationCircle');
@@ -102,8 +146,8 @@ function rightCol(audio, playIcon) {
         console.log('da click');
         if (!audio.paused) {
             audio.pause();
-            playIcon.classList.add("fa-play");
-            playIcon.classList.remove("fa-pause");
+            // playIcon.classList.add("fa-play");
+            // playIcon.classList.remove("fa-pause");
             rightPlay.classList.add("fa-play");
             rightPlay.classList.remove("fa-pause");
             animation.style.animationPlayState = 'paused';
@@ -111,8 +155,8 @@ function rightCol(audio, playIcon) {
         }
         else {
             audio.play();    
-            playIcon.classList.add("fa-pause");
-            playIcon.classList.remove("fa-play");
+            // playIcon.classList.add("fa-pause");
+            // playIcon.classList.remove("fa-play");
             rightPlay.classList.remove("fa-play");
             rightPlay.classList.add("fa-pause");
             console.log('da phat nhac roi');
@@ -125,8 +169,8 @@ function rightCol(audio, playIcon) {
         animation.style.animationPlayState = 'paused';
         rightPlay.classList.add("fa-play");
         rightPlay.classList.remove("fa-pause");
-        playIcon.classList.add("fa-play");
-        playIcon.classList.remove("fa-pause");
+        // playIcon.classList.add("fa-play");
+        // playIcon.classList.remove("fa-pause");
     });
 
     // xử lí thanh tiến trình nhạc 
